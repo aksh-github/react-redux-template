01bf2f2f393d4e81c8f77fbe5afd72910ab9e88f
Akshay Bapaye <1078306@TCSLANDG.net>
2019-11-12 21:43:20 +0000

    API call handling through Service middleware

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,4 +1,7 @@
 
+import { endPoints } from '../utils/config';
+
+
 export const FETCH_CURRENCY_FAIL = 'FETCH_CURRENCY_FAIL';
 
 export const currencyFetchFail = (error) =>
@@ -20,7 +23,7 @@ export const currencyFetch = () =>
         type: FETCH_CURRENCY,
         payload: {
             error: null,
-            loading: false
+            loading: true
         }
     };
 }
@@ -36,4 +39,95 @@ export const currencyFetchSuccess = (currency) =>
             value: currency
         }
     }
+}
+
+// export const fetchLatestCurrency = someApiRelated => (dispatch, getState) =>
+// {
+
+//     // const { isLoading, value } = getState().commonReducer.data.currency;
+
+//     // if (isLoading)
+//     // {
+//     //     console.log('Already Loading');
+//     //     return Promise.resolve(null);      //always return promise
+//     // }
+//     // else if (value)
+//     // {
+//     //     dispatch(currencyFetchSuccess(value))
+//     //     return Promise.resolve(value);      //always return promise
+//     // }
+
+//     dispatch(currencyFetch())
+
+//     //url should be in .env file
+
+//     return fetch('https://api.exchangeratesapi.io' + endPoints.latestCurrency)
+//         .then(resp => resp.json())
+//         .then((resp) =>
+//         {
+//             // console.log(resp);
+
+//             if (resp && resp.rates)
+//             {
+//                 const keys = Object.keys(resp.rates);
+//                 let arr = [];
+
+//                 for (let i = 0; i < keys.length; ++i)
+//                 {
+//                     let o = { [keys[i]]: resp.rates[keys[i]] }
+//                     arr.push(o)
+//                 }
+
+//                 dispatch(currencyFetchSuccess(arr))
+//             }
+//             else
+//                 dispatch(currencyFetchFail(resp.error))
+
+//             return resp;    //might be reqd some times
+//         })
+//         .catch((err) =>
+//         {
+//             console.log(err);
+//             dispatch(currencyFetchFail())
+//         })
+// }
+
+//use of middleware
+
+export const fetchCurrencyService = someApiRelated => (dispatch, getState) =>
+{
+
+    const transform = (resp) =>
+    {
+        if (resp && resp.rates)
+        {
+            const keys = Object.keys(resp.rates);
+            let arr = [];
+
+            for (let i = 0; i < keys.length; ++i)
+            {
+                let o = { [keys[i]]: resp.rates[keys[i]] }
+                arr.push(o)
+            }
+
+            return arr;
+        }
+    }
+
+    return dispatch({
+        type: 'API',
+        payload: {
+            api: {
+                url: 'https://api.exchangeratesapi.io' + endPoints.latestCurrency,
+                method: 'GET',
+            },
+            handlers: {
+                before: currencyFetch,
+                transform: transform,
+                onSuccess: currencyFetchSuccess,
+                onFailure: currencyFetchFail,
+                after: null
+            }
+        }
+    })
 }
