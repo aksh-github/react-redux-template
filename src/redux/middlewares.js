aa57390e7f93602c7b43abd131934a0be15dac62
Akshay Bapaye <1078306@TCSLANDG.net>
2019-11-22 22:16:38 +0000

    Middlewares refactoring All middlewares under 1 file intro responseValidator to decide dipatch success or failure

---
+++ src/redux/middlewares.js
@@ -0,0 +1,127 @@
+
+
+/**
+ * Logs all actions and states after they are dispatched.
+ */
+export const logger = store => next => action =>
+{
+    console.group(action.type)
+    console.info('dispatching', action)
+    // console.time('START');
+    let result = next(action)
+    console.log('next state', store.getState())
+    // console.timeEnd('START');
+    console.groupEnd()
+    return result
+}
+
+
+//Ajax middleware
+
+export const ServiceMiddleware = store => next => async action =>
+{
+    if (action.type !== 'API')
+    {
+        next(action);
+        return;
+    }
+
+    //middleware helper functions
+
+    const callToJson = async (result) =>
+    {
+        return result ? result.json() : Promise.resolve(result)
+    }
+
+    const callValidator = (result, responseValidator) =>
+    {
+        return responseValidator instanceof Function ? responseValidator(result) : true;
+    }
+
+    const callTransform = (result, transform) =>
+    {
+        if (transform instanceof Function)
+            return transform(result)
+        else
+            return result
+    }
+
+    //start middleware logic
+
+    let _result = null;
+
+    const { url, body, headers, method } = action.payload.api;
+
+    const {
+        before,     //before api call
+        responseValidator,  //to check whether response received is in correct format so that we can dispatch succcess action
+        transform,  //should return something
+        onSuccess,
+        errorTransform,
+        onFailure,
+        after       //after success or fail api call
+    } = action.payload.handlers;
+
+    const { dispatch } = store;
+
+    if (before instanceof Function)
+        dispatch(before());
+
+    try
+    {
+        _result = await fetch(url, {
+            method: method || 'GET',
+            body: JSON.stringify(body),
+            headers: {
+                //'custom-x': 'foobar',
+                ...headers  //this will retain (NOT overwrite) common headers 
+            }
+        });
+
+        //1. step 1: convert to json. If response is blank warn on console 
+        // result = await callToJson(result);
+
+        let result = await callToJson(_result)
+
+        //2. step 2: call validate when provided to decide to dispatch success or fail action
+        const validResponseFlag = callValidator(result, responseValidator)
+
+
+        // console.log(result, validResponseFlag)
+
+        //3. step 3: call transform when provided 
+        if (validResponseFlag)
+            result = callTransform(result, transform);
+
+        // console.log(result, validResponseFlag)
+
+        //4. dispatch success or fail based on response of validator
+
+        if (validResponseFlag)
+            dispatch(onSuccess(result))
+        else
+            dispatch(onFailure(result));
+
+        if (after instanceof Function)
+            after(result);
+
+        // console.log(result)
+
+        return result;
+
+    }
+    catch (err)
+    {
+        console.error(err);
+
+        if (errorTransform instanceof Function)
+            dispatch(onFailure(errorTransform(err)));
+        else
+            dispatch(onFailure(err));
+
+        if (after instanceof Function)
+            after(err);
+
+        return _result;
+    }
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -7,6 +7,8 @@ import commonReducer from './common-reducer';
 // import usersReducer from './users-reducer';
 // import aboutReducer from './about-reducer';
 
+import * as middlewares from './middlewares';
+
 
 const reducer = combineReducers({
     commonReducer: commonReducer,
@@ -15,92 +17,18 @@ const reducer = combineReducers({
     homeReducer: homeReducer
 });
 
-/**
- * Logs all actions and states after they are dispatched.
- */
-const logger = store => next => action =>
-{
-    console.group(action.type)
-    console.info('dispatching', action)
-    // console.time('START');
-    let result = next(action)
-    console.log('next state', store.getState())
-    // console.timeEnd('START');
-    console.groupEnd()
-    return result
-}
 
-const ServiceMiddleware = store => next => async action =>
-{
-    if (action.type !== 'API')
-    {
-        next(action);
-        return;
-    }
-
-    const { url, body, headers, method } = action.payload.api;
-
-    const {
-        before,     //before api call
-        transform,  //should return something
-        onSuccess,
-        errorTransform,
-        onFailure,
-        after       //after success or fail api call
-    } = action.payload.handlers;
-
-    const { dispatch } = store;
-
-    dispatch(before());
-
-    try
-    {
-        let result = await fetch(url, {
-            method: method || 'GET',
-            body: JSON.stringify(body),
-            headers: {
-                //'custom-x': 'foobar',
-                ...headers  //this will retain (NOT overwrite) common headers 
-            }
-        });
-
-        result = await result.json();
-
-        if (transform instanceof Function)
-            dispatch(onSuccess(transform(result)))
-        else
-            dispatch(onSuccess(result))
-
-
-        if (after instanceof Function)
-            after(result);
-
-        return result;
-    }
-    catch (err)
-    {
-        if (errorTransform instanceof Function)
-            dispatch(onFailure(errorTransform(err)));
-        else
-            dispatch(onFailure(err));
-
-        if (after instanceof Function)
-            after(err);
-    }
-};
-
-
-const middlewares = [thunk];
+const middlewaresArr = [thunk];
 
 if (process.env.NODE_ENV !== 'production')
-    middlewares.push(logger)
+    middlewaresArr.push(middlewares.logger)
 
 
-middlewares.push(ServiceMiddleware);
+middlewaresArr.push(middlewares.ServiceMiddleware);
 
 const store = createStore(
     reducer,
-    applyMiddleware(...middlewares)
+    applyMiddleware(...middlewaresArr)
 );
 
 export default store;
