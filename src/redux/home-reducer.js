c9ef1e4829da8e298d0a60a49f08f70431fb1193
Akshay Bapaye <1078306@TCSLANDG.net>
2019-12-06 12:34:49 +0000

    importing action types from actionTypes.js Using generic reducer (from redux-utils.js) to remove redundancy

--- src/redux/home-reducer.js
+++ src/redux/home-reducer.js
@@ -1,47 +1,24 @@
 
 import { homeState } from './initialState';
-import { FETCH_CURRENCY, FETCH_CURRENCY_FAIL, FETCH_CURRENCY_SUCCESS } from '../redux/action';
+// import { FETCH_CURRENCY, FETCH_CURRENCY_FAIL, FETCH_CURRENCY_SUCCESS } from '../redux/action';
 
-const updateCurrency = (state, action) =>
-{
-    switch (action.type)
-    {
-        case FETCH_CURRENCY:
-            return {
-                ...state,
-                loading: action.payload.loading,
-                error: action.payload.error
-            }
-        case FETCH_CURRENCY_FAIL:
-            return {
-                ...state,
-                loading: action.payload.loading,
-                error: action.payload.error
-            }
-        case FETCH_CURRENCY_SUCCESS:
-            return {
-                ...state,
-                loading: action.payload.loading,
-                error: action.payload.error,
-                value: state.value.concat(action.payload.value),
-            }
-        default:
-            return state;
-    }
-}
+import { fetchCurrencyType } from './actionTypes';
 
+import { createReducer } from './redux-utils';
+
+const currencyReducer = createReducer(fetchCurrencyType, 'MERGE')
 
 export default (state = homeState, action) =>
 {
 
     switch (action.type)
     {
-        case FETCH_CURRENCY:
-        case FETCH_CURRENCY_FAIL:
-        case FETCH_CURRENCY_SUCCESS:
+        case fetchCurrencyType.base:
+        case fetchCurrencyType.fail:
+        case fetchCurrencyType.success:
             return {
                 ...state,
-                currency: updateCurrency(state.currency, action)
+                currency: currencyReducer(state.currency, action)   //updateCurrency(state.currency, action)
             }
         default:
             return state;
