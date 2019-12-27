48be52227bf54b633867e6196df9d05348e80cb6
Akshay Bapaye <1078306@TCSLANDG.net>
2019-12-27 17:23:28 +0000

    Renamed action types

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -3,11 +3,14 @@
 
 import { createActionType, actionTypes } from './redux-utils';
 
-export const fetchCurrencyType = createActionType({ name: 'FETCH', group: 'CURRENCY', type: 'API' });
+export const currencyType = {
+    fetch: createActionType({ name: 'FETCH', group: 'CURRENCY', type: 'API' })
+}
 
-
-export const postTodoType = createActionType({ name: 'POST', group: 'TODO', type: 'API' });
-export const getTodoType = createActionType({ name: 'GET', group: 'TODO', type: 'API' });
+export const todoType = {
+    post: createActionType({ name: 'POST', group: 'TODO', type: 'API' }),
+    get: createActionType({ name: 'GET', group: 'TODO', type: 'API' })
+};
 
 export const incrementType = createActionType({ name: 'INCR_APICOUNTER' })
 export const decrementType = createActionType({ name: 'DECR_APICOUNTER' })
