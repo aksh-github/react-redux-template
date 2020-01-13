3f2574f858daeca7186412bf352b7cfaeb971e22
Akshay Bapaye <1078306@TCSLANDG.net>
2020-01-13 22:27:00 +0000

    Prettier formatting

--- src/ui/pages/home/Home.js
+++ src/ui/pages/home/Home.js
@@ -1,109 +1,104 @@
-
-
-import React from 'react';
+import React from "react";
 import { withRouter } from "react-router-dom";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import withHoc from '../../utils/hoc';
-import { Lazy } from '../../utils/Lazy';
+import withHoc from "../../utils/hoc";
+import { Lazy } from "../../utils/Lazy";
 
-
-import { fetchCurrencyService } from '../../../redux/action';
-import { postTodoService, getTodoService, incrementService } from '../../../redux/action2';
+import { fetchCurrencyService } from "../../../redux/action";
+import {
+    postTodoService,
+    getTodoService,
+    incrementService
+} from "../../../redux/action2";
 
 //import Currency from './comps/Currency';
 
-class Home extends React.Component
-{
-    static Currency = Lazy('page', 'home/comps/Currency');
+class Home extends React.Component {
+    static Currency = Lazy("page", "home/comps/Currency");
 
-    getCurrency = () =>
-    {
+    getCurrency = () => {
         // this.props.dispatch(fetchLatestCurrency())
         //     .then((resp) =>
         //     {
         //         // console.log(resp)
         //     })
 
-        this.props.fetchCurrencyService()
-            .then((resp) =>
-            {
-                console.log(resp)
+        this.props.fetchCurrencyService().then(resp => {
+            console.log(resp);
+        });
+    };
+
+    postTodo = () => {
+        const x = "test";
+
+        this.props
+            .postTodoService({
+                title: "foo",
+                body: "bar",
+                userId: 1
             })
-    }
+            .then(resp => {
+                console.log(resp);
+            });
+    };
 
-    postTodo = () =>
-    {
-
-        this.props.postTodoService({
-            "title": "foo",
-            "body": "bar",
-            "userId": 1
-        })
-            .then((resp) =>
-            {
-                console.log(resp)
-
-
+    getTodo = () => {
+        this.props
+            .getTodoService({
+                title: "foo",
+                body: "bar",
+                userId: 1
             })
-    }
+            .then(resp => {
+                console.log(resp);
+            });
+    };
 
-    getTodo = () =>
-    {
-        this.props.getTodoService({
-            "title": "foo",
-            "body": "bar",
-            "userId": 1
-        })
-            .then((resp) =>
-            {
-                console.log(resp)
-
-
-            })
-    }
-
-    render()
-    {
+    render() {
         const { currency, name } = this.props;
 
+        return (
+            <div>
+                <h2>Home {name}</h2>
+                <p>
+                    <button onClick={this.getCurrency}>Get remote data</button>
+                </p>
 
-        return <div>
-            <h2>Home {name}</h2>
-            <p><button onClick={this.getCurrency}>Get remote data</button></p>
+                <p>
+                    <button onClick={this.postTodo}>Post Todo</button>{" "}
+                    <button onClick={this.getTodo}>Get Todos</button>
+                </p>
 
-            <p><button onClick={this.postTodo}>Post Todo</button> <button onClick={this.getTodo}>Get Todos</button></p>
-
-            <Home.Currency currencyArr={currency.value} />
-        </div>
+                <Home.Currency currencyArr={currency.value} />
+            </div>
+        );
     }
 }
 
+const selectorHome = state => state.home;
 
-const selectorHome = (state) => state.home
-
-const mapStateToProps = (state) => 
-{
+const mapStateToProps = state => {
     const home = selectorHome(state);
 
     return {
         currency: home.currency,
         name: home.name
-    }
+    };
 };
 
-const mapDispatchToProps = (dispatch) =>
-{
+const mapDispatchToProps = dispatch => {
     return {
         fetchCurrencyService: () => dispatch(fetchCurrencyService()),
-        postTodoService: (todoObj) => dispatch(postTodoService(todoObj)),
-        getTodoService: () => dispatch(getTodoService()),
+        postTodoService: todoObj => dispatch(postTodoService(todoObj)),
+        getTodoService: () => dispatch(getTodoService())
         // incrementService: (v) => dispatch(incrementService(v))
-    }
-}
+    };
+};
 
 //export default withHoc(prop1, func)(withRouter(connect(mapStateToProps)(Home)));
-export default withRouter(withHoc('do nothing prop', (prop) =>
-{
-    console.log('do nothing func for HOC', prop);
-})(connect(mapStateToProps, mapDispatchToProps)(Home)));
+export default withRouter(
+    withHoc("do nothing prop", prop => {
+        console.log("do nothing func for HOC", prop);
+    })(connect(mapStateToProps, mapDispatchToProps)(Home))
+);
